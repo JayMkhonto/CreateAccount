8e78db12104d545f31ce6cdb2e58ed5c82ee64db
RC_Student_lab <RC_Student_lab@10.0.0.141>
2024-09-18 13:04:51 +0000

    Created Main Account Prompted user Created Login Class with methods

---
+++ src/main/java/com/mycompany/mainaccount/Login.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mainaccount;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+class Login {
+    //Check user name method
+    public boolean checkUserName(){
+        
+    }
+    //Check password complexity
+    public boolean checkPasswordComplexity(){
+        
+    }
+    //check register for user
+    public String registerUser(){
+        
+    }
+      //Check log in for user
+    public boolean loginUser(){
+        
+    }
+    //check log in status
+    public String returnLoginStatus(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/mainaccount/MainAccount.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mainaccount;
+import java.util.*;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class MainAccount {
+
+    public static void main(String[] args) {
+        //declaration
+        String firstName, lastName, username, password;
+        
+        //Object scanner
+        Scanner lg = new Scanner(System.in);
+       
+        //Prompting user to enter info
+        System.out.println("Enter first Name");
+        firstName = lg.next();
+        System.out.println("Enter last Name");
+        lastName = lg.next();
+        
+        System.out.println("Create Username");
+        username = lg.next();
+        System.out.println("Create Password");
+        password = lg.next();
+        
+        
+        //creating Login Class
+        Login lgn = new Login();
+        
+        
+        //Log in user
+        System.println.out("")
+        
+        
+        
+       
+    }
+}
